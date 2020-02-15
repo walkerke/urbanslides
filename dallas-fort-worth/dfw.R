00716e67ec443a98a7730fa3dcdce5f131217fa2
Kyle Walker <kwalkerdata@gmail.com>
2020-02-15 14:44:34 -0600

    updates to DFW slides

--- dallas-fort-worth/dfw.R
+++ dallas-fort-worth/dfw.R
@@ -2,13 +2,13 @@ library(tidycensus)
 library(tidyverse)
 library(extrafont)
 
-metro16 <- get_acs(geography = "metropolitan statistical area/micropolitan statistical area", 
+metro18 <- get_acs(geography = "cbsa", 
                    variables = "DP03_0019P", 
                    survey = "acs1", 
                    summary_var = "B01003_001") %>%
   filter(summary_est > 1500000) 
 
-metros <- metro16 %>%
+metros <- metro18 %>%
   mutate(type = ifelse(GEOID == "19100", "DFW", "Other")) %>%
   mutate(NAME = str_replace(NAME, "-.*", "")) %>%
   mutate(NAME = str_replace(NAME, ",.*|/.*", "")) %>%
@@ -18,17 +18,17 @@ color <- ifelse(metros$NAME == "Dallas", "navy", "grey30")
 face <- ifelse(metros$NAME == "Dallas", "bold", "plain")
 
 
-ggplot(metros, aes(x = estimate, y = reorder(NAME, estimate), 
+d1 <- ggplot(metros, aes(x = estimate, y = reorder(NAME, estimate), 
                    color = type)) + 
   geom_segment(aes(x = 0, y = reorder(NAME, estimate), 
                    xend = estimate, yend = reorder(NAME, estimate), 
                    color = type)) + 
   geom_point(size = 3) + 
-  theme_minimal(base_family = "Verdana") + 
+  theme_minimal(base_family = "Verdana", base_size = 16) + 
   scale_color_manual(values = c("navy", "#90b4d2"), guide = FALSE) + 
   scale_x_continuous(labels = function(x) { paste0(x, "%") }, 
                      expand = c(0.02, 0, 0.02, 0)) + 
-  labs(x = "2016 1-year ACS estimate", 
+  labs(x = "2018 1-year ACS estimate", 
        y = "", 
        title = "Percent driving alone to work", 
        subtitle = "Metro areas with populations above 1.5 million", 
@@ -40,16 +40,16 @@ ggplot(metros, aes(x = estimate, y = reorder(NAME, estimate),
         plot.caption = element_text(size = 7), 
         axis.text.y = element_text(color = color, face = face))
 
-ggsave("img/driving.png", width = 7, height = 7)
+ggsave("img/driving.png", d1, width = 11, height = 9)
 
 
-metro16 <- get_acs(geography = "metropolitan statistical area/micropolitan statistical area", 
+metro18 <- get_acs(geography = "cbsa", 
                    variables = "DP03_0024P", 
                    survey = "acs1", 
                    summary_var = "B01003_001") %>%
   filter(summary_est > 1500000) 
 
-metros <- metro16 %>%
+metros <- metro18 %>%
   mutate(type = ifelse(GEOID == "19100", "DFW", "Other")) %>%
   mutate(NAME = str_replace(NAME, "-.*", "")) %>%
   mutate(NAME = str_replace(NAME, ",.*|/.*", "")) %>%
@@ -69,7 +69,7 @@ ggplot(metros, aes(x = estimate, y = reorder(NAME, estimate),
   scale_color_manual(values = c("navy", "#90b4d2"), guide = FALSE) + 
   scale_x_continuous(labels = function(x) { paste0(x, "%") }, 
                      expand = c(0.02, 0, 0.02, 0)) + 
-  labs(x = "2016 1-year ACS estimate", 
+  labs(x = "2018 1-year ACS estimate", 
        y = "", 
        title = "Percent working at home", 
        subtitle = "Metro areas with populations above 1.5 million", 
@@ -89,10 +89,10 @@ place00 <- get_decennial(geography = "place", state = "TX",
   mutate(NAME = str_replace(NAME, " city.*", "")) %>%
   select(NAME, pop00 = value)
 
-place16 <- get_acs(geography = "place", state = "TX", 
+place18 <- get_acs(geography = "place", state = "TX", 
                    variables = "B01003_001", survey = "acs1") %>%
   mutate(NAME = str_replace(NAME, " city.*", "")) %>%
-  select(NAME, pop16 = estimate) %>%
+  select(NAME, pop18 = estimate) %>%
   inner_join(place00, by = "NAME")
 
 dfwp <- c("Allen", "Arlington", "Carrollton", "Dallas", "Denton", 
@@ -100,20 +100,20 @@ dfwp <- c("Allen", "Arlington", "Carrollton", "Dallas", "Denton",
          "Irving", "Lewisville", "McKinney", "Mansfield", 
          "Mesquite", "Plano", "Richardson")
 
-dfw <- filter(place16, NAME %in% dfwp)
+dfw <- filter(place18, NAME %in% dfwp)
 
 p <- ggplot(dfw) + 
-  geom_segment(aes(x = pop00, xend = pop16, 
-                   y = reorder(NAME, pop16), 
-                   yend = reorder(NAME, pop16))) + 
-  geom_point(aes(x = pop16, y = reorder(NAME, pop16), text = paste0("2016: ", pop16)), 
+  geom_segment(aes(x = pop00, xend = pop18, 
+                   y = reorder(NAME, pop18), 
+                   yend = reorder(NAME, pop18))) + 
+  geom_point(aes(x = pop18, y = reorder(NAME, pop18), text = paste0("2018: ", pop18)), 
              color = "navy", size = 3) + 
-  geom_point(aes(x = pop00, y = reorder(NAME, pop16), text = paste0("2000: ", pop00)), 
+  geom_point(aes(x = pop00, y = reorder(NAME, pop18), text = paste0("2000: ", pop00)), 
              color = "#90b4d2", size = 3) + 
   geom_text(data = data.frame(), aes(x = 1188580, y = "Dallas", label = "2000"), 
             color = "#90b4d2", hjust = 1, size = 3, fontface = "bold", 
             nudge_x = -80000) + 
-  geom_text(data = data.frame(), aes(x = 1317942, y = "Dallas", label = "2016"), 
+  geom_text(data = data.frame(), aes(x = 1317942, y = "Dallas", label = "2018"), 
             color = "navy", hjust = 0, size = 3, fontface = "bold", 
             nudge_x = 80000) + 
   scale_x_continuous(labels = scales::comma, expand = c(0.1, 0.1)) + 
