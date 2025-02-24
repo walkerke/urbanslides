a8a887fde73acc40c9d8c6c7ba0342970a1f5cff
Kyle Walker <kyle@walker-data.com>
2025-02-24 12:21:39 -0600

    update dfw slides

--- dallas-fort-worth/dfw.R
+++ dallas-fort-worth/dfw.R
@@ -6,7 +6,7 @@ metro21 <- get_acs(geography = "cbsa",
                    variables = "DP03_0019P", 
                    survey = "acs1", 
                    summary_var = "B01003_001",
-                   year = 2021) %>%
+                   year = 2023) %>%
   filter(summary_est > 1500000) 
 
 metros <- metro21 %>%
@@ -29,7 +29,7 @@ d1 <- ggplot(metros, aes(x = estimate, y = reorder(NAME, estimate),
   scale_color_manual(values = c("navy", "#90b4d2"), guide = FALSE) + 
   scale_x_continuous(labels = function(x) { paste0(x, "%") }, 
                      expand = c(0.02, 0, 0.02, 0)) + 
-  labs(x = "2021 1-year ACS estimate", 
+  labs(x = "2023 1-year ACS estimate", 
        y = "", 
        title = "Percent driving alone to work", 
        subtitle = "Metro areas with populations above 1.5 million", 
@@ -92,7 +92,7 @@ place00 <- get_decennial(geography = "place", state = "TX",
   select(NAME, pop00 = value)
 
 place21 <- get_acs(geography = "place", state = "TX", 
-                   variables = "B01003_001", survey = "acs1", year = 2021) %>%
+                   variables = "B01003_001", survey = "acs1", year = 2023) %>%
   mutate(NAME = str_replace(NAME, " city.*", "")) %>%
   select(NAME, pop21 = estimate) %>%
   inner_join(place00, by = "NAME")
@@ -108,14 +108,14 @@ p <- ggplot(dfw) +
   geom_segment(aes(x = pop00, xend = pop21, 
                    y = reorder(NAME, pop21), 
                    yend = reorder(NAME, pop21))) + 
-  geom_point(aes(x = pop21, y = reorder(NAME, pop21), text = paste0("2021: ", pop21)), 
+  geom_point(aes(x = pop21, y = reorder(NAME, pop21), text = paste0("2023: ", pop21)), 
              color = "navy", size = 3) + 
   geom_point(aes(x = pop00, y = reorder(NAME, pop21), text = paste0("2000: ", pop00)), 
              color = "#90b4d2", size = 3) + 
   geom_text(data = data.frame(), aes(x = 1188580, y = "Dallas", label = "2000"), 
             color = "#90b4d2", hjust = 1, size = 3, fontface = "bold", 
             nudge_x = -80000) + 
-  geom_text(data = data.frame(), aes(x = 1317942, y = "Dallas", label = "2021"), 
+  geom_text(data = data.frame(), aes(x = 1317942, y = "Dallas", label = "2023"), 
             color = "navy", hjust = 0, size = 3, fontface = "bold", 
             nudge_x = 80000) + 
   scale_x_continuous(labels = scales::comma, expand = c(0.1, 0.1)) + 
